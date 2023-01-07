8474c0e1146c15a65e7de771fa8c1ef6758ea6f9
majidaguebguabi <122041540+majidaguebguabi@users.noreply.github.com>
2023-01-07 10:54:26 -0800

    Update NewScene.h

--- Classes/NewScene.h
+++ Classes/NewScene.h
@@ -1,27 +1,11 @@
-#ifndef __NEW_SCENE_H__
-#define __NEW_SCENE_H__
-
 #include "cocos2d.h"
 
-class NewScene : public cocos2d::Layer
+class NewScene : public cocos2d::Scene
 {
 public:
-    // there's no 'id' in cpp, so we recommend returning the class instance pointer
     static cocos2d::Scene* createScene();
 
-    // Here's a difference. Method 'init' in cocos2d-x returns bool, instead of returning 'id' in cocos2d-iphone
-    virtual bool init();  
-    
-    // a selector callback
-    void menuCloseCallback(cocos2d::Ref* pSender);
-    
-    // implement the "static create()" method manually
-    CREATE_FUNC(NewScene);
-    
-    cocos2d::Sprite *mySprite;
-    
-    void GoBack(Ref *pSender);
-    
-};
+    virtual bool init();
 
-#endif // __NEW_SCENE_H__
+    CREATE_FUNC(NewScene);
+};
